6aef43820a9740be6aa8b7015217873ea2210744
dlgustj206 <dlgustj206@naver.com>
2025-05-15 19:55:26 +0900

    chore: Prisma 쿼리 로깅 옵션 추가

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
@@ -39,30 +38,4 @@ app.post("/api/missions/:missionId/challenge", handleChallengeMission);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
-*/
-import express from 'express';
-
-const app = express();
-const port = 3000;
-
-const myLogger = (req, res, next) => {
-    console.log("LOGGED");
-
-}
-
-app.use(myLogger);
-
-app.get('/', (req, res) => {
-    console.log("/");
-    res.send('Hello UMC!');
-});
-
-app.get('/hello', (req, res) => {
-    console.log("/hello");
-    res.send('Hello world!');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
 });
