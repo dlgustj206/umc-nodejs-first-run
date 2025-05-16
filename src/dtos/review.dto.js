c74fac52c610707d671f04bb45eec6346c8a8d28
dlgustj206 <dlgustj206@naver.com>
2025-05-16 18:55:03 +0900

    feat: 리뷰 목록 응답 포맷 함수 responseFromReviews 작성

--- src/dtos/review.dto.js
+++ src/dtos/review.dto.js
@@ -22,3 +22,24 @@ export const responseFromReview = (review) => {
         updatedAt: review.updated_at,
     };
 };
+
+export const responseFromReviews = (reviews) => {
+    if (!reviews) {
+        return null;
+    }
+
+    return {
+        data: reviews.map(review => ({
+            id: Number(review.id),
+            body: review.body,
+            score: review.score,
+            userId: review.user_id ? Number(review.user_id) : null,
+            storeId: review.store_id ? Number(review.store_id) : null,
+            createdAt: review.created_at,
+            updatedAt: review.updated_at
+        })),
+        pagenation: {
+            cursor: reviews.length ? Number(reviews[reviews.length - 1].id) : null
+        }
+    };
+};
