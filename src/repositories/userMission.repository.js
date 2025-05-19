8458ec1a8123db74e2a493213247efbb83af6f17
dlgustj206 <dlgustj206@naver.com>
2025-05-19 22:36:38 +0900

    feat: userMission.reporitory.js 코드 Prisma 기반으로 리팩토링

--- src/repositories/userMission.repository.js
+++ src/repositories/userMission.repository.js
@@ -1,49 +1,49 @@
-import { pool } from "../db.config.js";
+import { prisma } from "../db.config.js";
 
-export const addUserMission = async (missionData) => {
-    const conn = await pool.getConnection();
-
+export const addUserMission = async (data) => {
     try {
-        const [result] = await conn.query(
-            'INSERT INTO user_mission (user_id, mission_id, status) VALUES (?, ?, ?)',
-            [missionData.user_id, missionData.mission_id, missionData.status]
-        );
-        return result.insertId;
+        const mission = await prisma.userMission.create({
+            data: {
+                user_id: data.user_id,
+                mission_id: data.mission_id,
+                status: data.status,
+                created_at: new Date(),
+                updated_at: new Date()
+            }
+        })
+        return mission.id;
     } catch (err) {
         throw new Error('DB 오류 발생: ' + err.message);
-    } finally {
-        conn.release();
     }
 };
 
 export const getUserMissionById = async (missionId) => {
-    const conn = await pool.getConnection();
-
     try {
-        const [userMission] = await pool.query('SELECT * FROM user_mission WHERE id = ?;', [missionId]);
-        
-        if (missionId.length == 0) {
-            return null;
+        const userMission = await prisma.userMission.findUnique({
+            where: { id: missionId },
+            include: {
+                user: true,
+                mission: true
+            }
+        })
+
+        if(!userMission) {
+            throw new Error("존재하지 않는 미션입니다");
         }
 
-        return userMission[0];
+        return userMission.id;
     } catch (err) {
         throw new Error('진행 중인 미션 조회 실패: ' + err.message);
-    } finally {
-        conn.release();
     }
-} 
+};
 
 export const isMissionAlreadyChallenged = async (userId, missionId) => {
-    const conn = await pool.getConnection();
-
-    try {
-        const [rows] = await conn.query(
-            'SELECT id FROM user_mission WHERE user_id =? AND mission_id =?',
-            [userId, missionId]
-        );
-        return rows.length > 0;
-    } finally {
-        conn.release();
-    }
+    const mission = await prisma.userMission.findUnique({
+        where: {
+            user_id: userId,
+            mission_id: missionid
+        },
+        select: { id: true }
+    });
+    return mission;
 };
